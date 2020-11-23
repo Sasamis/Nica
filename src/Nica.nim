3f8fbbf180a97271490f874c07a7b4de8c294006
Momeemt <momiimt@gmail.com>
2020-11-23 11:12:30 +0900

    ✨  Add: Matrix-type subtraction implemented
    Matrix型の減算を実装

--- src/Nica.nim
+++ src/Nica.nim
@@ -2,17 +2,29 @@ type Matrix* = seq[seq[int]]
 type MatrixDifferentRowsError* = object of CatchableError
 type MatrixDifferentColumnsError* = object of CatchableError
 
-proc `+`* (left, right: Matrix): Matrix =
+proc sameStructure (left, right: Matrix) =
   if left.len != right.len:
     raise newException(MatrixDifferentRowsError, "Matrix-type additions must have the same number of rows.")
   for index in 0..<left.len:
     if left[index].len != right[index].len:
       raise newException(MatrixDifferentColumnsError, "Matrix-type additions must have the same number of columns.")
+
+proc `+`* (left, right: Matrix): Matrix =
+  sameStructure left, right
   result = @[]
   for index1 in 0..<left.len:
     var row: seq[int] = @[]
     for index2 in 0..<left[index1].len:
       row.add left[index1][index2] + right[index1][index2]
+    result.add row
+
+proc `-`* (left, right: Matrix): Matrix =
+  sameStructure left, right
+  result = @[]
+  for index1 in 0..<left.len:
+    var row: seq[int] = @[]
+    for index2 in 0..<left[index1].len:
+      row.add left[index1][index2] - right[index1][index2]
     result.add row
 
 proc hello* (): string =
--- tests/test_nica.nim
+++ tests/test_nica.nim
@@ -23,3 +23,22 @@ suite "Matrix type addiction":
     const right: Nica.Matrix = @[@[7, 8], @[9, 10]]
     expect Nica.MatrixDifferentColumnsError:
       discard left + right
+
+suite "Matrix type subtraction":
+  test "Succeed in subtraction":
+    const left: Nica.Matrix = @[@[1, 2], @[3, 4]]
+    const right: Nica.Matrix = @[@[5, 6], @[7, 8]]
+    const answer: Nica.Matrix = @[@[-4, -4], @[-4, -4]]
+    check left - right == answer
+
+  test "Failure to subtruct different numbers of rows in matrix type":
+    const left: Nica.Matrix = @[@[1, 2], @[3, 4], @[5, 6]]
+    const right: Nica.Matrix = @[@[7, 8], @[9, 10]]
+    expect Nica.MatrixDifferentRowsError:
+      discard left - right
+
+  test "Failure to subtract different numbers of columns in matrix type":
+    const left: Nica.Matrix = @[@[1, 2, 3], @[4, 5, 6]]
+    const right: Nica.Matrix = @[@[7, 8], @[9, 10]]
+    expect Nica.MatrixDifferentColumnsError:
+      discard left - right
